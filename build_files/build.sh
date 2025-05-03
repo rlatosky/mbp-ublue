2bbd842a4bd3557be32fab10b9d9fb78c431379e
rlatosky <rlatosky@posteo.net>
2025-05-03 10:30:11 -0400

    Add akmod through build.sh instead...

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,6 +19,28 @@ dnf5 install -y tmux timg
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+
+if [[ "${RELEASE}" -ge 41 ]]; then
+    COPR_RELEASE="rawhide"
+else
+    COPR_RELEASE="${RELEASE}"
+fi
+
+curl -LsSf -o /etc/yum.repos.d/_copr_mulderje-facetimehd-kmod.repo \
+    "https://copr.fedorainfracloud.org/coprs/mulderje/facetimehd-kmod/repo/fedora-${COPR_RELEASE}/mulderje-facetimehd-kmod-fedora-${COPR_RELEASE}.repo"
+
+### BUILD facetimehd (succeed or fail-fast with debug output)
+dnf install -y \
+    akmod-facetimehd-*.fc${RELEASE}.${ARCH}
+akmods --force --kernels "${KERNEL}" --kmod facetimehd
+modinfo "/usr/lib/modules/${KERNEL}/extra/facetimehd/facetimehd.ko.xz" > /dev/null \
+|| (find /var/cache/akmods/facetimehd/ -name \*.log -print -exec cat {} \; && exit 1)
+
+rm -f /etc/yum.repos.d/_copr_mulderje-facetimehd-kmod.repo
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
